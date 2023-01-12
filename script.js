e3c6339c8f7109392cac9881070103b514660908
Davor Budimir <47507294+BudimirDavor@users.noreply.github.com>
2023-01-12 23:15:30 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ window.onload = function()
 	let k = document.getElementById("kuna");
 	e.oninput = function() 
 	{
-		k.value = (e.value * 7.5345);
+		k.value = (e.value * 7.5345).toFixed(4);
 	};
 	k.oninput = function() 
 	{
-		e.value = (k.value / 7.5345);
+		e.value = (k.value / 7.5345).toFixed(4);
 	}
 };
