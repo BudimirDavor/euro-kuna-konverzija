e985798b507ebca61b908a921bd87b9818ec0160
Davor Budimir <47507294+BudimirDavor@users.noreply.github.com>
2023-01-12 21:28:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+window.onload = function() 
+{
+	let e = document.getElementById("euro");
+	let k = document.getElementById("kuna");
+	e.oninput = function() 
+	{
+		k.value = (e.value * 7.5345);
+	};
+	k.oninput = function() 
+	{
+		e.value = (k.value / 7.5345);
+	}
+};
